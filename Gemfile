2dbd21dc853c8ef7e8f252e91b2f953e34df876f
Brennen Berkley <hello@brennenberkley.com>
2018-11-19 09:01:38 -0800

    Set up capistrano with reachnetwork.com

--- Gemfile
+++ Gemfile
@@ -12,15 +12,22 @@ group :development do
   gem 'pry-byebug'
 
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-end
 
-group :production do
-  gem 'unicorn'
+  gem "capistrano"
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
+  gem 'capistrano3-puma'
+
+  gem "awesome_print"
+  gem "binding_of_caller"
+  gem 'web-console'
+
+  gem 'reach_linters', git: 'git@github.com:reachnetwork/reach_linters.git'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ config/deploy/production.rb
@@ -0,0 +1,42 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+
+server  'charon-reach.reachnetwork.com',
+        user: 'capuser',
+        roles: %w[web app],
+        ssh_options: {
+          forward_agent: true
+        }
+
+set :branch, 'master'
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
+role :app, "review-bot.reachnetwork.com"
+role :web, "review-bot.reachnetwork.com"
+
+set :rbenv_type, :user # Defaults to: :auto
+set :rbenv_ruby, File.read('.ruby-version').strip # Defaults to: 'default'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
+
+# NOTE : You cannot have this uncommented. We've had it in previous deploy files, but with Puma it causes an issue. The default value coming from Capistrano is correct, and it should just be allowed to use that.
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}# honeybadger sidekiq sidekiqctl
+
+set :puma_threads, [4, 16]
+set :puma_workers, 2
